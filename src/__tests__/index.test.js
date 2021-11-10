8f917ffb15478a0fb084f0d233efea8b45c6eeb4
Sneha Devi <sneha.devi@dsinnovators.com>
2021-11-10 15:19:13 +0600

    Add few success testcases

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -6,34 +6,181 @@ import MultiLevelTable from '../index'
 const data = [
   {
     id: 1,
-    institution_name: 'Birshreshtha Noor Mohammad Public College',
-    institution_name_bn: 'বীরশ্রেষ্ঠ নূর মোহাম্মদ পাবলিক কলেজ'
+    name: "Lorem University ",
+    degrees: [
+      {
+        id: 1,
+        name: "B.Sc ",
+        courses: [
+          {
+            id: 1,
+            name: "Computer Science and Engineering ",
+          },
+          {
+            id: 2,
+            name: "Electrical and Electronics Engineering",
+          },
+          {
+            id: 3,
+            name: "Busniess Administration",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "M.Sc",
+        courses: [
+          {
+            id: 1,
+            name: "Computer Science and Engineering",
+          },
+          {
+            id: 2,
+            name: "Electrical and Electronics Engineering",
+          },
+        ],
+      },
+    ],
   },
   {
     id: 2,
-    institution_name: 'Govt. Mohammadpur Model School & College',
-    institution_name_bn: 'সরকারি মোহাম্মদপুর মডেল স্কুল অ্যান্ড কলেজ'
+    name: "Ipsum University",
+    degrees: [
+      {
+        id: 1,
+        name: "B.Sc",
+        courses: [
+          {
+            id: 1,
+            name: "Computer Science and Engineering",
+          },
+          {
+            id: 2,
+            name: "Busniess Administration",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "M.Sc",
+        courses: [
+          {
+            id: 1,
+            name: "Computer Science and Engineering",
+          },
+        ],
+      },
+    ],
   },
   {
     id: 3,
-    institution_name: 'Viqarunnisa Noon School & College',
-    institution_name_bn: 'ভিকারুননিসা নূন স্কুল এন্ড কলেজ'
+    name: "Test University"
   }
-]
+];
 
 const structure = {
-  name: 'institutions',
-  children: [
-    { field: 'institution_name', display: 'Name' },
-    { field: 'institution_name_bn', display: 'Name (BN)' }
+    name: "institutions",
+    children: [{
+      field: "name",
+      display: "Institute"
+    }],
+    array: {
+      name: "degrees",
+      children: [{
+        field: "name",
+        display: "Degree"
+      }],
+      array: {
+        name: "courses",
+        children: [{
+          field: "name",
+          display: "Course"
+        }],
+      },
+    },
+  };
+
+const actions = [
+    {
+      name: "View Course Details",
+      callback: (value) => {
+        console.log("Course Details :", value);
+      },
+    },
+    {
+      name: "Students",
+      callback: (value) => {
+        console.log("Students:", value);
+      },
+    },
   ]
-}
 
-test('loads and displays greeting', () => {
-  render(<MultiLevelTable data={data} structure={structure} />)
 
-  expect(screen.getByRole('columnheader', { name: 'Name' })).toBeInTheDocument()
+const actionLabel = 'Manage '
+
+test('header name testing ', () => {
+  render(
+    <MultiLevelTable
+      data={data}
+      structure={structure}
+      actions={actions}
+      actionLabel={actionLabel}
+    />
+  )
+
+  expect(screen.getByRole('columnheader', { name: 'Institute' })).toBeInTheDocument()
   expect(
-    screen.getByRole('columnheader', { name: 'Name (BN)' })
+    screen.getByRole('columnheader', { name: 'Course' })
   ).toBeInTheDocument()
 })
+
+test('header testing ', () => {
+  render(
+    <MultiLevelTable
+      data={data}
+      structure={structure}
+      actions={actions}
+      actionLabel={actionLabel}
+    />
+  )
+
+  const input = screen.getByRole('row', { name: /Institute/i })
+  expect(input.textContent).toContain('Degree')
+  expect(input.textContent).toContain('Course')
+  expect(input.textContent).toContain('Manage')
+})
+
+test('checking valid row values', () => {
+  render(
+    <MultiLevelTable
+      data={data}
+      structure={structure}
+      actions={actions}
+      actionLabel={actionLabel}
+    />
+  )
+
+  const row = screen.getByRole('row', { name: /Lorem University/i })
+  expect(row.textContent).toContain('B.Sc Computer Science and Engineering')
+  expect(row.textContent).toContain('Manage ↓')
+})
+
+
+test('checking rowspan of institute names', () => {
+  render(
+    <MultiLevelTable
+      data={data}
+      structure={structure}
+      actions={actions}
+      actionLabel={actionLabel}
+    />
+  )
+
+  const cell = screen.getByRole('cell', { name: /Lorem University/i })
+  expect(cell.rowSpan).toBe(5)
+})
+
+
+
+
+
